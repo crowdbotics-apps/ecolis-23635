c057e363c8d695edae6faaac18adbc85b18752f7
Crowdbotics <team@crowdbotics.com>
2021-01-01 07:42:14 +0000

    add package blueprint-react-native-settings-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Settings189656Navigator from '../features/Settings189656/navigator';
 
 /**
  * new navigators can be imported here
@@ -13,6 +14,7 @@ import SideMenu from './sideMenu';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Settings189656: { screen: Settings189656Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
